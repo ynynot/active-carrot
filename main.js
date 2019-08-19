0776f114ed03eeee897d130a5d017bb59e18a1d7
ynynot <47995027+ynynot@users.noreply.github.com>
2019-08-19 15:11:42 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, initial-scale=1";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
