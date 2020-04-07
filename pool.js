7088ec479cd7945da5f21b61d6eeab2c9ee41a3c
ynynot <47995027+ynynot@users.noreply.github.com>
2020-04-07 23:43:30 +1000

    Create pool.js

---
+++ pool.js
@@ -0,0 +1,9 @@
+if (location.pathname.indexOf('customer/dashboard') === -1) {
+
+var bookOnline = document.getElementById("title_nav").getElementsByClassName("page_heading")[0];
+bookOnline.innerHTML = "Pool Availability";
+
+var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
+facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Facility Booking", "Pool Availbility");
+
+}
