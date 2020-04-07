2e4f8f46506e84f9891e3e207ce59b5a2747b512
ynynot <47995027+ynynot@users.noreply.github.com>
2020-04-07 23:46:43 +1000

    Update pool.js

--- pool.js
+++ pool.js
@@ -6,4 +6,4 @@ bookOnline.innerHTML = "Pool Availability";
 var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
 facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Facility Booking", "Pool Availbility");
 
-}
+};
